1303f1514624718312eb567a373bced14ba034c7
Shaqnero <ssenya001@st.ug.edu.gh>
2024-07-04 23:13:39 +0000

    Final Year Project

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Router from "./routes/Router";
 import { AppProvider } from "./context/Context";
 import { RootSiblingParent } from "react-native-root-siblings";
